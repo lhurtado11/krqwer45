b77b4f85fa3d2d85ae1c2d19c33aebecba0a24f0
Luisa <lhurtado372@gmail.com>
2021-05-20 21:00:55 -0500

    reto tareas completadas superado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-
-/// Modifica el componente para que se puedan agregar tareas, tachar y destacharlas y error de validacion en el input
+import './index.css'
 
 class App extends Component {
   constructor() {
@@ -14,16 +13,54 @@ class App extends Component {
       newTask: ''
     }
   }
+
+  changeDone = (index) => {
+    this.setState ({
+      tasks: this.state.tasks.map( e => {
+        if (e.id === index) {
+          e.done = !e.done
+        }
+        return e
+      })
+    }) 
+  }
+
+  onSubmit(e) {
+    if(this.state.newTask === '') {
+
+    } else {
+      const task = {
+        id:   this.state.tasks.length + 1,
+        name: this.state.newTask,
+        done: false
+      }
+
+      console.log(task, e.target.value)
+      this.setState({
+        tasks: [...this.state.tasks, task],
+        newTask: ''
+      })
+    }
+    
+    e.preventDefault()
+  }
+
+  handleChange(e) {
+    this.setState({
+      newTask: e.target.value
+    })
+  } 
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
+            {this.state.tasks.map((task) => <li className={task.done ? 'done' : null} onClick={() => this.changeDone(task.id)} key={task.id}>{task.name}</li>)}
           </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
+          <form onSubmit={this.onSubmit.bind(this)}>
+            <input className={!this.state.newTask ? 'error' : null} type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} onChange={this.handleChange.bind(this)} />
           </form>
         </div>
       </div>
